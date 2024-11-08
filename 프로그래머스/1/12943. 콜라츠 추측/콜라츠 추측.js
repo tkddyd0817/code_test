9da15cd23beab497f4ae4249336da59d70a5d858
tkddyd0817 <tkddyd1259@gmail.com>
2024-11-08 09:33:25 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 32.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,13 @@
+function solution(num) {
+    var count = 0;
+
+    while (count < 500) {
+        if (num === 1) {
+            return count;
+        }
+        count ++;
+        num = num % 2 === 0 ? num /2 : num *3 +1;
+    }
+
+    return -1;
+}
